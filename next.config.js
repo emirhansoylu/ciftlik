a29304ac91fd86b54900189b32807a8d99ab144d
Emirhan Soylu <emirhansoylu98@gmail.com>
2025-04-08 19:30:32 +0300

    Fix resource problem online

--- next.config.js
+++ next.config.js
@@ -3,7 +3,11 @@ const nextConfig = {
   output: 'export',
   images: {
     unoptimized: true
-  }
+  },
+  basePath: process.env.NODE_ENV === 'production' ? '/ciftlik' : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/ciftlik/' : '',
+  trailingSlash: true,
+  distDir: 'out'
 }
 
 module.exports = nextConfig 
